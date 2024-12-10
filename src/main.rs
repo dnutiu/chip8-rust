680ee8adeb02bb94097a6e1f0a38e2f9ae017138
Denis Nutiu <denis.nutiu@gmail.com>
2024-12-10 18:45:46 +0200

    add command line arguments via clap crate

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use crate::display::RatatuiDisplay;
 use crate::emulator::Emulator;
 use crate::input::CrossTermInput;
 use crate::sound::TerminalSound;
+use clap::Parser;
 use env_logger;
-use std::env;
-use std::path::PathBuf;
 
 mod display;
 mod emulator;
@@ -13,12 +12,23 @@ mod instruction;
 mod sound;
 mod stack;
 
+#[derive(Parser, Debug)]
+#[command(
+    version = "1.0",
+    about = "A Chip8 emulator.",
+    long_about = "A program which emulates the Chip8 system."
+)]
+struct CliArgs {
+    /// The path to the ROM file to emulate.
+    rom_path: String,
+}
+
 fn main() -> Result<(), anyhow::Error> {
     env_logger::init();
-    let rom_path = PathBuf::from(env::args().skip(1).next().expect("rom path not provided."));
+    let args = CliArgs::parse();
 
     let mut emulator = Emulator::new(RatatuiDisplay::new(), TerminalSound, CrossTermInput::new());
-    emulator.emulate(rom_path.to_str().unwrap())?;
+    emulator.emulate(args.rom_path)?;
 
     Ok(())
 }
